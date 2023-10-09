98317bb188e30b5c5a21971598972b4cf7eed3b2
Tanisha-Raorane1284 <122742837+Tanisha-Raorane1284@users.noreply.github.com>
2023-10-09 20:51:05 +0530

    Add files via upload

---
+++ stories webpage/script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulated data (you can replace this with actual data)
+    const totalDonors = 100;
+    const peopleHelped = 250;
+
+    // Update the counters with the simulated data
+    document.getElementById('donor-count').textContent = totalDonors;
+    document.getElementById('people-helped-count').textContent = peopleHelped;
+});
